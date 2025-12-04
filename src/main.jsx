3eb27f61dbdfbc5435478eb9ed6383335f6c2c6a
Sagnik Nandy <sagniknandy27@gmail.com>
2025-12-04 18:26:24 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import store from "./Redux/store.js";
 import "../src/i18n.js";
-import CacheBuster from "react-cache-buster";
-import VersionCheck from "./components/VersionCheck/VersionCheck.jsx";
-import { version } from "../package.json";
+import { CacheBuster } from '@piplup/cache-buster';
 
 // The env should always in the root of my project else it will show undefined
 const CLIENTID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
@@ -33,14 +31,17 @@ registerServiceWorker();
 const current_mode = "production";
 const isProduction = current_mode === "production";
 
+// @piplup/vite-plugin-cache-buster
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <CacheBuster
-    currentVersion={version}
-    isEnabled={isProduction} //If false, the library is disabled.
-    isVerboseMode={false} //If true, the library writes verbose logs to console.
-    loadingComponent={<h1>Version checking from app.....</h1>} //If not pass, nothing appears at the time of new version check.
-    metaFileDirectory={"."} //If public assets are hosted somewhere other than root on your server.
+    // Only enable in production build
+    // enabled={import.meta.env.PROD}
+    enabled={true}
+    loading={null}
+    verbose={false}
+    storageKey="Iqbook-key" 
   >
     <GoogleOAuthProvider clientId={CLIENTID}>
       <Provider store={store}>
