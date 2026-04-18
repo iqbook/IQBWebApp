bcf7f9545e5ab558bd86654518f953b342c37ef0
Sagnik Nandy <sagniknandy27@gmail.com>
2026-04-18 18:48:15 +0530

    commit

--- src/Admin/Salon/CreateSalon/CreateSalon.jsx
+++ src/Admin/Salon/CreateSalon/CreateSalon.jsx
@@ -1708,8 +1708,7 @@ const CreateSalon = () => {
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    // googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    googleMapsApiKey: "AIzaSyBvLAyt0bJxFNWQx01bhbxqGYh-wNHgFIs",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
     libraries: ["places"],
   });
 
